73dde5e3dc1086254e3aa22a72f21c6b6c92a7b1
James T <jtara@tuta.io>
2017-12-14 14:55:26 -0800

    add cutoff_probability; update and fix to handle various inputs; add input validation

--- src/bernoulli.js
+++ src/bernoulli.js
@@ -26,31 +26,39 @@ function n_choose_r(n, r) {
     return Math.trunc(numerator / denominator);
 }
 
-function bernoulli_trials(trials, prob_of_success) {
+function bernoulli_trials(trials, prob_of_success, cutoff_probability) {
     var divider = "----------------------------------------";
-    var successes = 0;
     var prob = 1;
+    var previous_probability = 1;
     var probabilities = [];
     var output = ["trials = " + trials + ", success = " + prob_of_success,
                   divider]
     var prob_of_failure = 1 - prob_of_success;
 
-    while ((prob >= 0.0001 || successes <= 2) && trials >= successes) {
-        prob = n_choose_r(trials, successes)
+    var calc_probability = (successes) => (n_choose_r(trials, successes)
             * Math.pow(prob_of_success, successes)
-            * Math.pow(prob_of_failure, trials - successes);
-        prob = prob.toPrecision(3);
+            * Math.pow(prob_of_failure, trials - successes)).toPrecision(3);
+
+    for (var successes = 0; successes <= trials; successes++) {
+        prob = calc_probability(successes);
+        if (cutoff_probability > prob) {
+            if (prob < previous_probability) {
+                break;
+            } else if (prob > previous_probability) {
+                continue;
+            }
+        }
+        previous_probability = prob;
         output.push("successes = " + successes + ", probability = " + prob);
 
-        successes++;
-        probabilities.push(prob);
+        probabilities.push([successes, prob]);
     }
 
-    var add = (x, y) => (Number(x) + Number(y)).toPrecision(3);
+    var add = (x, y) => (Number(x) + Number(y[1])).toPrecision(3);
     output.push(divider);
-    for (var i = 1; i < probabilities.length; i++) {
-        output.push("successes >= " + i + ", probability = "
-            + probabilities.slice(i).reduce(add));
+    for (var i = 1; i < probabilities.length - 1; i++) {
+        output.push("successes >= " + probabilities[i][0] + ", probability = "
+            + probabilities.slice(i).reduce(add, 0));
     }
     return output;
 }
@@ -62,14 +70,16 @@ function bernoulli_trials(trials, prob_of_success) {
  */
 function bernoulli_calc() {
     // trials
-    var t = Number(document.forms.namedItem("bernoulli_form")
-                   .children.namedItem("trials").value);
+    var trials = Number(document.getElementById("trials").value);
 
     // probability of a single success
-    var s = Number(document.forms.namedItem("bernoulli_form")
-                   .children.namedItem("success").value);
+    var success = Number(document.getElementById("success").value);
+    var cutoff_probability = Number(document.getElementById("cutoff_probability").value);
 
-    var bernoulli_info = bernoulli_trials(t, s);
+    if (invalid_form_data(trials, success, cutoff_probability)) {
+        return true;
+    }
+    var bernoulli_info = bernoulli_trials(trials, success, cutoff_probability);
 
     // div element reserved for bernolli output
     var div = document.getElementById("bernoulli_output");
@@ -78,9 +88,9 @@ function bernoulli_calc() {
     var br = document.createElement("br");
     // render output from bernoulli_trials in HTML
     for (var i = 0; i < bernoulli_info.length; i++) {
-        var ele = document.createElement("p");
-        ele.textContent = bernoulli_info[i];
-        div.appendChild(ele);
+        var p_element = document.createElement("p");
+        p_element.textContent = bernoulli_info[i];
+        div.appendChild(p_element);
         div.appendChild(br);
     }
     return false;
@@ -89,5 +99,18 @@ function bernoulli_calc() {
 function remove_children(node) {
     while (node.firstChild) {
         node.removeChild(node.firstChild);
+    }
+}
+
+function invalid_form_data(trials, success, cutoff) {
+        if (trials < 0) {
+        alert("trials must be >= 0");
+        return true;
+    } else if (!(1 > success > 0)) {
+        alert("success must be in range [0, 1]");
+        return true;
+    } else if (!(1 > cutoff > 0)) {
+        alert("success must be in range [0, 1]");
+        return true;
     }
 }
