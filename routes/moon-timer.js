d1938cca8db58eedc92944109818b2feab8e8851
James T <github.jtara1@gmail.com>
2019-03-01 23:29:38 -0800

    chg: clean up some code

--- routes/moon-timer.js
+++ routes/moon-timer.js
@@ -1,11 +1,6 @@
-// const EventEmitter = require('events');
-// const { join } = require('path');
-
 const moment = require('moment');
-// const player = require('play-sound')(opts = {});
 
-// const logger = require('./logger')(module);
-const logger = {
+const logger = { // "logger" that's compatible in any js context without dependencies
   print(msg, obj) {
     console.log(msg, JSON.stringify(obj));
   },
@@ -23,26 +18,13 @@ const logger = {
 moment().format();
 
 const config = {
-  warningMinutes: '0.25',
+  warningMinutes: '0.25', // time before start of a moon phase that warning is sent out
   loglevel: 'info',
   resetMinutes: 55,
   firstCycleTime: '14 Aug 2018 17:22:00 GMT',
-  fullMoonRole: 'FullMoon',
-  newMoonRole: 'NewMoon',
-  allowRoleManagement: 'true',
-  allowImageCache: '1',
-  imagesAsThumbnails: '1',
-  showImagesVelocityCheckSeconds: '60',
-  delayBetweenImagesMs: 1050,
-  assignFloors: [50, 49, 48, 47, 46]
 };
 
-// const soundFile = join(__dirname, '..', 'complete.ogg');
-
-// const eventEmitter = new EventEmitter();
-
 const fullMoonRoleMention = '';
-const newMoonRoleMention = '';
 
 let currentCycleStart = moment(config.firstCycleTime);
 
@@ -53,8 +35,6 @@ let nextFullmoonWarning;
 let nextFullmoon;
 
 const warningMinutesOffset = config.warningMinutes * -1;
-
-const { resetMinutes } = config;
 
 // Hide now() behind a function for easy testing
 function GetNow() {
@@ -114,7 +94,7 @@ function InitialiseNewMoonTime() {
 function GetTimeLeft(endTime) {
   const timeLeft = moment.duration(endTime.diff(GetNow()));
   const timeLeftFormatted = moment(timeLeft._data).format('hh:mm:ss');
-  return `${timeLeft.hours()}h${timeLeft.minutes()}m${timeLeft.seconds()}s`;
+  return `${timeLeft.hours()}h ${timeLeft.minutes()}m ${timeLeft.seconds()}s`;
 }
 
 function FullMoonLoop() {
@@ -123,7 +103,7 @@ function FullMoonLoop() {
     logger.debug('processing...');
     now = GetNow();
     if (now >= nextFullmoonWarning) {
-      logger.verbose(`${fullMoonRoleMention} Time until next Full Moon :${GetTimeLeft(nextFullmoon)}`);
+      logger.verbose(`Time until next Full Moon :${GetTimeLeft(nextFullmoon)}`);
       nextFullmoonWarning.add(118, 'm');
       promptTimeout('Full Moon').catch(console.error);
 
@@ -131,7 +111,7 @@ function FullMoonLoop() {
     }
 
     if (now >= nextFullmoon) {
-      logger.verbose(`${fullMoonRoleMention} Currently in Full moon! 2x MAG is live!`);
+      logger.verbose(`Currently in Full moon! 2x MAG is live!`);
       nextFullmoon.add(118, 'm');
 
       LogTimes();
@@ -163,7 +143,6 @@ function FullMoonLoop() {
 function SendTimeTillFullMoon() {
   if (moment.duration(nextFullmoon.diff(GetNow())).asMinutes() > 108) {
     logger.verbose('Currently in Full Moon! 2x Mag is live!');
-    // eventEmitter.emit('fullMoon');
   }
 
   logger.verbose(`Time until next Full Moon :${GetTimeLeft(nextFullmoon)}`);
@@ -172,7 +151,6 @@ function SendTimeTillFullMoon() {
 function SendTimeTillNewMoon() {
   if (moment.duration(nextCycleStart.diff(GetNow())).asMinutes() > 108) {
     logger.verbose('Currently in New Moon! 2x EXP is live!');
-    // eventEmitter.emit('newMoon');
   }
 
   logger.verbose(`Time until next New Moon :${GetTimeLeft(nextCycleStart)}`);
@@ -198,6 +176,8 @@ function alertPopup(msg) {
 }
 
 async function promptTimeout(msg, timeoutTime = 60) {
+  if (!this.hasOwnProperty('window') || !window) return;
+
   if (!this.openPrompt) this.openPrompt = true;
   else if (this.openPrompt) return; // avoid spamming prompts every hour or so
 
@@ -210,8 +190,5 @@ async function promptTimeout(msg, timeoutTime = 60) {
     })
   ]);
 }
-
-// eventEmitter.on('fullMoon', () => {});
-// eventEmitter.on('newMoon', () => {});
 
 Initialise(); // entry point
