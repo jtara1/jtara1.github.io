bd007a5e0317af5e92bf96cf9e5d888a27fa374d
James T <github.jtara1@gmail.com>
2021-08-13 01:04:21 -0700

    feat: (wip) updated nav bar to just be nav pill item links, centered, slightly bigger font

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import Container from 'react-bootstrap/Container';
 import {
-  Col,
   Nav,
   Navbar,
   Row
@@ -12,16 +11,21 @@ import './App.css';
 import { WEBSITE_TITLE } from './config';
 
 const MainNavBar = () => {
-  const style = { border: '5px solid' };
+  const style = {
+    // border: '5px solid',
+    fontSize: 24,
+    marginTop: '20px',
+  };
 
   return (
-    <Navbar id="main-nav-bar" bg="light" expand="xl">
-      {/*<Container fluid="xl" style={{ width: '100px' }}>*/}
-      <Container fluid="xl">
-        <Nav href="#home" style={style}>Home</Nav>
-        <Nav href="#blogs" style={style}>Blogs</Nav>
-      </Container>
-    </Navbar>
+      <Nav fluid style={{ border: '5px solid'}} className="justify-content-center" variant="pills" defaultActiveKey="home-link">
+        <Nav.Item>
+          <Nav.Link style={style} eventKey="home-link">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link style={style} eventKey="blogs-link">Blogs</Nav.Link>
+        </Nav.Item>
+      </Nav>
   );
 };
 
@@ -29,7 +33,7 @@ const App = () => (
   <>
     <MainNavBar />
     <Container className="p-3">
-       <Row>
+      <Row>
          <h1 className="header">Welcome to {WEBSITE_TITLE}</h1>
        </Row>
     </Container>
