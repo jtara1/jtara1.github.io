9c4e7d709b795973815db72c29bedf59f110f03d
James T <github.jtara1@gmail.com>
2021-08-19 18:51:16 -0700

    feat(AboutMe): added p tag and some info on myself

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,5 +1,17 @@
+import Container from 'react-bootstrap/Container';
+
+
 const AboutMe = () => {
-  return <div>about me</div>;
+  return (
+    <Container className="p-3">
+      <p>
+        Hello, I'm James.<br />
+
+        I work as a Software Engineer, and I enjoy playing tennis &#127934;, video games
+        &#128126;, and studying &#128214;.
+      </p>
+    </Container>
+  );
 };
 
 export default AboutMe;
