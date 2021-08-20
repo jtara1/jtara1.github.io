6519f23d2673420c08707a807770a887564278df
James T <github.jtara1@gmail.com>
2021-08-19 18:25:55 -0700

    feat(App,MainNavbar): added usage of react-router-dom so we aren't loading an entirely new html page on each click

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import './App.css';
 import MainNavbar from './layout/MainNavbar';
 import Home from './pages/Home';
+import AboutMe from './pages/AboutMe';
 
 
 const App = () => (
   <>
-    <MainNavbar />
-    <Home />
+    <BrowserRouter>
+      <MainNavbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about-me">
+          <AboutMe />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   </>
 );
 
--- src/layout/MainNavbar.js
+++ src/layout/MainNavbar.js
@@ -1,6 +1,7 @@
 import { Nav, Navbar } from 'react-bootstrap';
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import { Link } from 'react-router-dom';
 
 import classes from './MainNavBar.module.css';
 
@@ -9,12 +10,24 @@ const MainNavbar = () => {
   return (
     <Navbar className={classes.bar}>
       <Container>
-        <Navbar.Brand href="/" className={classes.brand}>jtara1</Navbar.Brand>
+        <Navbar.Brand className={classes.brand}>
+          <Link stlye={{ all: "inherit" }} to="/">
+            jtara1
+          </Link>
+        </Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse>
           <Nav>
-            <Nav.Link href="/about-me" className={classes.link}>About Me</Nav.Link>
-            <Nav.Link href="/loot-box-calc" className={classes.link}>Loot Box Calc</Nav.Link>
+            <Nav.Link className={classes.link}>
+              <Link stlye={{ all: "inherit" }} to="/about-me">
+                About Me
+              </Link>
+            </Nav.Link>
+            <Nav.Link href="/loot-box-calc" className={classes.link}>
+              <Link stlye={{ all: "inherit" }} to="/loot-box-calc">
+                Loot Box Calc
+              </Link>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
